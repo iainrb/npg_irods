056fd65a1ff99ed75cc03cea5ed0b77d4025336c
iainrb <ib5@sanger.ac.uk>
2017-03-24 11:36:48 +0000

    Additional BioNano publisher tests

--- t/lib/WTSI/NPG/OM/BioNano/RunPublisherTest.pm
+++ t/lib/WTSI/NPG/OM/BioNano/RunPublisherTest.pm
@@ -11,7 +11,7 @@ use URI;
 
 use base qw[WTSI::NPG::HTS::Test]; # FIXME better path for shared base
 
-use Test::More tests => 7;
+use Test::More tests => 13;
 use Test::Exception;
 
 use English qw[-no_match_vars];
@@ -32,12 +32,17 @@ BEGIN { use_ok('WTSI::NPG::OM::BioNano::RunPublisher'); }
 use WTSI::NPG::iRODS;
 use WTSI::NPG::OM::BioNano::RunPublisher;
 
+# $REAL_USER_ID used by NPG::Annotator, but causes Travis test to fail
+my $user_name = getpwuid $EFFECTIVE_USER_ID;
+my $affiliation_uri = URI->new('http://www.sanger.ac.uk');
+
 my $data_path = './t/data/bionano';
-my $runfolder_name = 'stock_barcode_01234_2016-10-04_09_00';
+my @runfolder_names = qw[stock_barcode_01234_2016-10-04_09_00
+                         stock_barcode_56789_2016-10-05_12_00];
+my @test_run_paths;
 my $fixture_path = "t/fixtures";
 my $tmp_data;
 my $tmp_db;
-my $test_run_path;
 my $irods_tmp_coll;
 my $pid = $$;
 my $wh_schema;
@@ -72,15 +77,17 @@ sub make_fixture : Test(setup) {
     # workaround for the space in BioNano's "Detect Molecules" directory,
     # because Build.PL does not work well with spaces in filenames
     $tmp_data = tempdir('temp_bionano_data_XXXXXX', CLEANUP => 1);
-    my $run_path = $data_path.'/'.$runfolder_name;
-    system("cp -R $run_path $tmp_data") && $log->logcroak(
-        q[Failed to copy '], $run_path, q[' to '], $tmp_data, q[']);
-    $test_run_path = $tmp_data.'/'.$runfolder_name;;
-    $test_run_path = $tmp_data.'/'.$runfolder_name;
-    my $cmd = q[mv ].$test_run_path.q[/Detect_Molecules ].
-        $test_run_path.q[/Detect\ Molecules];
-    system($cmd) && $log->logcroak(
-        q[Failed rename command '], $cmd, q[']);
+    foreach my $runfolder_name (@runfolder_names) {
+        my $run_path = $data_path.'/'.$runfolder_name;
+        system("cp -R $run_path $tmp_data") && $log->logcroak(
+            q[Failed to copy '], $run_path, q[' to '], $tmp_data, q[']);
+        my $test_run_path = $tmp_data.'/'.$runfolder_name;
+        my $cmd = q[mv ].$test_run_path.q[/Detect_Molecules ].
+            $test_run_path.q[/Detect\ Molecules];
+        system($cmd) && $log->logcroak(
+            q[Failed rename command '], $cmd, q[']);
+        push @test_run_paths, $test_run_path;
+    }
 }
 
 sub teardown : Test(teardown) {
@@ -92,7 +99,7 @@ sub publish : Test(2) {
     my $irods = WTSI::NPG::iRODS->new();
     my $publication_time = DateTime->now;
     my $publisher = WTSI::NPG::OM::BioNano::RunPublisher->new(
-        directory => $test_run_path,
+        directory => $test_run_paths[0],
         publication_time => $publication_time,
         mlwh_schema => $wh_schema,
     );
@@ -109,14 +116,14 @@ sub publication_results : Test(4) {
     my $irods = WTSI::NPG::iRODS->new();
     my $publication_time = DateTime->new(
         year       => 2016,
-        month      => 1,
+        month      => 11,
         day        => 1,
         hour       => 12,
         minute     => 00,
     );
 
     my $publisher = WTSI::NPG::OM::BioNano::RunPublisher->new(
-        directory => $test_run_path,
+        directory => $test_run_paths[0],
         mlwh_schema => $wh_schema,
     );
     my $bionano_obj = $publisher->publish($irods_tmp_coll,
@@ -162,10 +169,7 @@ sub publication_results : Test(4) {
     is_deeply(\@contents, $expected_contents,
               '.tar.gz archive contents match expected values');
 
-    # find values to check metadata
-    # $REAL_USER_ID used by NPG::Annotator, but causes Travis test to fail
-    my $user_name = getpwuid $EFFECTIVE_USER_ID;
-    my $affiliation_uri = URI->new('http://www.sanger.ac.uk');
+    # check md5 in metadata
     my $md5 = Digest::MD5->new;
     $md5->add(read_file($bionano_copy));
     my $md5sum = $md5->hexdigest();
@@ -193,7 +197,7 @@ sub publication_results : Test(4) {
         },
         {
             'attribute' => 'dcterms:created',
-            'value' => '2016-01-01T12:00:00'
+            'value' => '2016-11-01T12:00:00'
         },
         {
             'attribute' => 'dcterms:creator',
@@ -263,6 +267,177 @@ sub publication_results : Test(4) {
             'attribute' => 'type',
             'value' => 'tar'
         },
+    );
+
+    is_deeply(\@object_meta, \@expected_meta,
+              "Object metadata matches expected values");
+
+}
+
+sub dies_with_malformed_runfolder_name : Test(1) {
+    my $run_copy_dir = $tmp_data.'/bad_name';
+    mkdir $run_copy_dir ||
+        $log->logcroak('Failed to create directory ', $run_copy_dir);
+    my $bad_name = 'not-a-well-formatted-bionano-name';
+    my $dest = $run_copy_dir.'/'.$bad_name;
+    system('cp -R '.$test_run_paths[0].' '.$dest) &&
+        $log->logcroak('Failed to copy ', $test_run_paths[0],
+                       ' to ', $dest);
+    dies_ok {
+        my $publisher = WTSI::NPG::OM::BioNano::RunPublisher->new(
+            directory => $dest,
+            mlwh_schema => $wh_schema,
+        );
+        $publisher->publish($irods_tmp_coll);
+    }, 'Dies with malformed runfolder name';
+}
+
+sub dies_without_bnx : Test(1) {
+    my $run_copy_dir = $tmp_data.'/no_bnx';
+    mkdir $run_copy_dir ||
+        $log->logcroak('Failed to create directory ', $run_copy_dir);
+    system('cp -R '.$test_run_paths[0].' '.$run_copy_dir) &&
+        $log->logcroak('Failed to copy ', $test_run_paths[0],
+                       ' to ', $run_copy_dir);
+    my $run_path = $run_copy_dir.'/'.$runfolder_names[0];
+    unlink $run_path.'/Detect Molecules/Molecules.bnx' ||
+        $log->logcroak('Failed to delete Molecules.bnx');
+    dies_ok {
+        my $publisher = WTSI::NPG::OM::BioNano::RunPublisher->new(
+            directory => $run_path,
+            mlwh_schema => $wh_schema,
+        );
+        $publisher->publish($irods_tmp_coll);
+    }, 'Dies without a Molecules.bnx file';
+}
+
+sub publish_without_stock_meta : Test(4) {
+    # runfolder stock_barcode_56789_2016-10-05_09_00 has no dummy MLWH entry
+    # check on publication results; sample/study metadata will be absent
+
+    my $irods = WTSI::NPG::iRODS->new();
+    my $publication_time = DateTime->new(
+        year       => 2016,
+        month      => 11,
+        day        => 1,
+        hour       => 12,
+        minute     => 00,
+    );
+
+    my $bionano_obj;
+    my $uuid;
+    do {
+        # object will warn about missing sample/study; suppress screen output
+        local *STDERR;
+        open STDERR, '>', '/dev/null' ||
+            $log->logcroak("Failed to open /dev/null for STDERR");
+        my $publisher = WTSI::NPG::OM::BioNano::RunPublisher->new(
+            directory => $test_run_paths[1],
+            mlwh_schema => $wh_schema,
+        );
+        $bionano_obj = $publisher->publish($irods_tmp_coll,
+                                           $publication_time);
+        $uuid = $publisher->uuid;
+        close STDERR ||
+            $log->logcroak("Failed to close /dev/null for STDERR");
+    };
+    my $bionano_copy = abs_path($tmp_data.'/bionano_published.tar.gz');
+    $irods->get_object($bionano_obj, $bionano_copy);
+
+    # check contents of tarfile
+    my $tar = Archive::Tar->new;
+    $tar->read($bionano_copy);
+    my @contents = $tar->list_files();
+    is(scalar @contents, 28, 'Expected number of files in .tar.gz archive');
+
+    my $expected_contents = [
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Molecules.bnx',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/RawMolecules.bnx',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/RawMolecules1.bnx',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/RawMolecules2.bnx',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/RawMolecules3.bnx',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/RawMolecules4.bnx',
+          'stock_barcode_56789_2016-10-05_12_00/Metadata.xml',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/RunReport.txt',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Stitch.fov',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/iovars.json',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Stitch1.fov',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Stitch2.fov',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Stitch3.fov',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Stitch4.fov',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Molecules1.mol',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Molecules2.mol',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Molecules3.mol',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Molecules4.mol',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Labels1.lab',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Labels2.lab',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Labels3.lab',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Labels4.lab',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Molecules.mol',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/Labels.lab',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/iovars1.json',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/iovars2.json',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/iovars3.json',
+          'stock_barcode_56789_2016-10-05_12_00/Detect Molecules/iovars4.json'
+        ];
+    is_deeply(\@contents, $expected_contents,
+              '.tar.gz archive contents match expected values');
+
+    # check md5 in metadata
+    my $md5 = Digest::MD5->new;
+    $md5->add(read_file($bionano_copy));
+    my $md5sum = $md5->hexdigest();
+
+    my @object_meta = $irods->get_object_meta($bionano_obj);
+
+    is(scalar @object_meta, 11,
+       "Expected number of collection AVUs found");
+
+    my @expected_meta = (
+        {
+            'attribute' => 'bnx_chip_id',
+            'value' => '20000,10000,1/1/2015,987654321'
+        },
+        {
+            'attribute' => 'bnx_flowcell',
+            'value' => 1
+        },
+        {
+            'attribute' => 'bnx_instrument',
+            'value' => 'B001'
+        },
+        {
+            'attribute' => 'bnx_uuid',
+            'value' => $uuid,
+        },
+        {
+            'attribute' => 'dcterms:created',
+            'value' => '2016-11-01T12:00:00'
+        },
+        {
+            'value' =>  $affiliation_uri,
+            'attribute' => 'dcterms:creator'
+        },
+        {
+            'attribute' => 'dcterms:publisher',
+            'value' =>  'ldap://ldap.internal.sanger.ac.uk/ou=people,dc=sanger,dc=ac,dc=uk?title?sub?(uid='.$user_name.')',
+        },
+        {
+            'attribute' => 'md5',
+            'value' => $md5sum,
+        },
+        {
+            'value' => 'production',
+            'attribute' => 'source'
+        },
+        {
+            'attribute' => 'stock_id',
+            'value' => 'stock_barcode_56789'
+        },
+        {
+            'attribute' => 'type',
+            'value' => 'tar'
+        }
     );
 
     is_deeply(\@object_meta, \@expected_meta,
