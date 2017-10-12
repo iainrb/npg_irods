b31d7bb0c02628d57935066da1ca1edc40726641
iainrb <ib5@sanger.ac.uk>
2017-10-12 14:03:46 +0100

    Update metadata and remove script tests

--- t/lib/WTSI/NPG/OM/BioNano/RunPublisherTest.pm
+++ t/lib/WTSI/NPG/OM/BioNano/RunPublisherTest.pm
@@ -7,7 +7,7 @@ use URI;
 
 use base qw[WTSI::NPG::HTS::Test]; # FIXME better path for shared base
 
-use Test::More;
+use Test::More tests => 7;
 use Test::Exception;
 
 use English qw[-no_match_vars];
@@ -125,7 +125,7 @@ sub metadata : Test(4) {
                                            $publication_time);
     my @collection_meta = $irods->get_collection_meta($bionano_coll);
 
-    is(scalar @collection_meta, 20,
+    is(scalar @collection_meta, 21,
        "Expected number of collection AVUs found");
 
     my @expected_meta = (
@@ -160,6 +160,10 @@ sub metadata : Test(4) {
         {
             'attribute' => 'sample',
             'value' => '425STDY6079620'
+        },
+        {
+            'value' => 'ERS1791246',
+            'attribute' => 'sample_accession_number'
         },
         {
             'attribute' => 'sample_cohort',
@@ -257,6 +261,10 @@ sub metadata : Test(4) {
             'value' => '425STDY6079620'
         },
         {
+            'attribute' => 'sample_accession_number',
+            'value' => 'ERS1791246'
+        },
+        {
             'attribute' => 'sample_cohort',
             'value' => 'Virus_6'
         },
@@ -310,56 +318,11 @@ sub metadata : Test(4) {
         },
     );
 
-    is(scalar @file_meta, 22, "Expected number of BNX file AVUs found");
+    is(scalar @file_meta, 23, "Expected number of BNX file AVUs found");
 
     is_deeply(\@file_meta, \@expected_file_meta,
               'BNX file metadata matches expected values');
 }
 
-sub script : Test(10) {
-
-    my $irods = WTSI::NPG::iRODS->new();
-
-    system("find $test_run_path -exec touch {} +") && $log->logcroak(
-        "Failed to recursively update access time for $test_run_path"
-    );
-
-    my $script = "npg_publish_bionano_run.pl";
-
-    my $cmd = "$script --collection $irods_tmp_coll --search_dir $tmp_data";
-
-    ok(system($cmd)==0, "Publish script run successfully with search dir");
-
-    my $expected_coll = $irods_tmp_coll."/d5/0f/b6/".$runfolder_name;
-    ok($irods->is_collection($expected_coll),
-       "Script publishes to expected iRODS collection");
-
-    my $expected_bnx = $expected_coll."/Detect Molecules/Molecules.bnx";
-    ok($irods->is_object($expected_bnx),
-       "Script publishes expected filtered BNX file");
-
-    $irods->remove_collection($expected_coll);
-
-    $cmd = "$script --collection $irods_tmp_coll --search_dir $tmp_data ".
-        "--runfolder_path $test_run_path 2> /dev/null";
-    ok(system($cmd)!=0, "Publish script fails with incompatible arguments");
-    ok(! $irods->is_collection($expected_coll),
-       "No iRODS collection published by failed script");
-
-    $cmd = "$script --collection $irods_tmp_coll --runfolder_path ".
-        "$tmp_data/foo/bar 2> /dev/null";
-    ok(system($cmd)!=0, "Publish script has non-zero exit status for ".
-           "nonexistent input");
-    ok(! $irods->is_collection($expected_coll),
-       "No iRODS collection published by failed script");
-
-    $cmd = "$script --collection $irods_tmp_coll ".
-        "--runfolder_path $test_run_path";
-    ok(system($cmd)==0, "Publish script run successfully with runfolder");
-    ok($irods->is_collection($expected_coll),
-       "Script publishes to expected iRODS collection");
-    ok($irods->is_object($expected_bnx),
-       "Script publishes expected filtered BNX file");
-}
 
 1;
